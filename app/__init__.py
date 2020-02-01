c02722c11e7537afa59b7c9cfe25fd003a14b13e
habibahassan <halimaadan92@gmail.com>
2020-02-01 13:41:36 +0300

    added index.html on templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
-from .config import DevConfig
+# from flask import Flask
+# from .config import DevConfig
 
-# Initializing application
-app = Flask(__name__)
+# # Initializing application
+# app = Flask(__name__)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
+# # Setting up configuration
+# app.config.from_object(DevConfig)
 
-from app import views
+# from app import views
