ec9a6f435d3f4280a98d6e528c96028d920f97f6
habibahassan <halimaadan92@gmail.com>
2020-02-03 11:17:26 +0300

    creating basic flask application

--- app/main/views.py
+++ app/main/views.py
@@ -1,28 +1,30 @@
-from flask import render_template
-from app import app
+# from flask import render_template,redirect,url_for,request
+# from . import main
+# from ..models import Sources,Articles
+# from ..request import get_articles,get_sources
 
-# Views
-@app.route('/')
-def index():
+# @main.route('/')
+# def index():
+#     '''
+#     Function that returns the index page and the processed data
+#     '''
+#     general_news = get_sources('general')
+#     business_news = get_sources('business')
+#     entertainment_news = get_sources('entertainment')
+#     sports_news = get_sources('sports')
+#     technology_news = get_sources('technology')
+   
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-
+#     title = 'Home | Best News Update Site'
     
-    return render_template('index.html')
-@app.route('/news/<int:news_id>')
-def movie(news_id):
+#     return render_template('index.html',title=title, general=general_news, business=business_news, entertainment=entertainment_news, sports=sports_news, technology=technology_news)
 
-    '''
-    View news page function that returns the news details page and its data
-    '''
-    return render_template('news.html',id = news_id)
-def index():
+# @main.route('/articles/<id>')
+# def articles(id):
+# 	'''
+# 	Function to view articles page
+# 	'''
+# 	news_articles = get_articles(id)
+# 	title = f'{id}'
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-
-    title = 'Home - Welcome to The best News Review Website Online'
-    return render_template('index.html', title = title)
+# 	return render_template('articles.html',title= title,articles = news_articles)
