891608115eacb8e9443da764bc1e1227ec1a7778
habibahassan <halimaadan92@gmail.com>
2020-02-03 11:22:23 +0300

    created get sources functions on request.py

--- app/request.py
+++ app/request.py
@@ -1,35 +1,35 @@
-# import urllib.request,json
-# from .models import Sources,Articles
+import urllib.request,json
+from .models import Sources,Articles
 
-# # Getting api key
-# api_key = None
-# # Getting the news sources url
-# base_url = None
-# #Getting the news articles url
-# articles_url = None
+# Getting api key
+api_key = None
+# Getting the news sources url
+base_url = None
+#Getting the news articles url
+articles_url = None
 
 
-# def configure_request(app):
-#     global api_key,base_url,articles_url
-#     api_key = app.config['NEWS_API_KEY']
-#     base_url = app.config['NEWS_API_BASE_URL']
-#     articles_url = app.config['NEWS_ARTICLES_URL']
-# def get_sources(category):
-#     '''
-#     Function that gets the json response to our url request
-#     '''
-#     get_sources_url = base_url.format(category,api_key)
+def configure_request(app):
+    global api_key,base_url,articles_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
+    articles_url = app.config['NEWS_ARTICLES_URL']
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(category,api_key)
 
-#     with urllib.request.urlopen(get_sources_url) as url:
-#         get_sources_data = url.read()
-#         get_sources_response = json.loads(get_sources_data)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
 
-#         sources_results = None
+        sources_results = None
 
-#         if get_sources_response['sources']:
-#             sources_results = process_results(get_sources_response['sources'])
+        if get_sources_response['sources']:
+            sources_results = process_results(get_sources_response['sources'])
     
-#     return sources_results
+    return sources_results
 
 # def process_results(news_sources_list):
 #     '''
